2085fe737ea618249c73f9a1a5bce6d828edecf8
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-24 17:26:55 +0000

    Extensions - added route to single order

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,13 @@ from models.online_shop import all_orders
 @app.route("/orders")
 def index():
     return render_template("index.html", title="Home", all_orders=all_orders)
+
+
+
+# Extension - Create a new route
+# <index> is the value we type in the browser, e.g.  /1
+@app.route("/orders/<index>")
+def order(index):
+    index_num = int(index)
+    order = all_orders[index_num]
+    return render_template("order.html", title="Single order", order=order)
